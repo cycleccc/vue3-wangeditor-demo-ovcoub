d1aec215ef3d377903952ee66fc27c8a6d9297c3
cycleccc <2991205548@qq.com>
2024-11-19 21:38:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue3-wangeditor-demo-ovcoub
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/cycleccc/vue3-wangeditor-demo-ovcoub)
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <p>Vue3 wangEditor demo</p>
+    <BasicEditor></BasicEditor>
+  </div>
+</template>
+
+<script>
+import BasicEditor from './components/BasicEditor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BasicEditor,
+  },
+};
+</script>
+
+<style></style>
